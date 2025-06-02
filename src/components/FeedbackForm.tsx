6b54b56f72502f4cb0e5793b4fd88722ea0996a8
truejojo <johannesveh@yahoo.de>
2025-06-02 12:19:22 +0200

    Add counter to FeedbackForm

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import { MAX_CHARS_LENGTH } from '../lib/constants';
+
 const FeedbackForm = () => {
+  const [textarea, setTextarea] = useState<string>('');
+
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    setTextarea(event.target.value);
+  };
+
+  const charCount = MAX_CHARS_LENGTH - textarea.length;
+
   return (
     <form className='form'>
+      <textarea
+        id='feedback-textarea'
+        value={textarea}
+        onChange={handleTextareaChange}
+        placeholder='Default placeholder text for showing the text from label'
+        spellCheck={false}
+      />
       <label htmlFor='feedback-textarea'>
         Enter your feedback here, remember to #hashtag the company
       </label>
-      <textarea id='feedback-textarea' spellCheck={false} />
       <div>
-        <p className='u-italic'>150</p>
+        <p className='u-italic'>{charCount}</p>
         <button type='button'>
           <span>Submit</span>
         </button>
---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const MAX_CHARS_LENGTH = 150;
