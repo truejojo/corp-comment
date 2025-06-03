56017ffcb6c0e3859e8614277fd852d10f1c475c
truejojo <johannesveh@yahoo.de>
2025-06-03 07:49:47 +0200

    Add Spinner and ErrorMessage component and add Error & Loading handling to FeedbackList

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = ({ message }: { message: string }) => {
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,37 +1,48 @@
 import { useEffect, useState } from 'react';
 import FeedbackItem from './FeedbackItem';
 import type { FeedbackItemProps } from '../types';
+import Spinner from './Spinner';
+import ErrorMessage from './ErrorMessage';
 
 const FeedbackList = () => {
   const [feedbackItems, setFeedbackItems] = useState<FeedbackItemProps[] | []>(
     [],
   );
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch(
-      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks',
-    )
-      .then((response) => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks',
+        );
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data.feedbacks);
+        const data = await response.json();
         setFeedbackItems(data.feedbacks);
-      })
-      .catch((error) => {
-        console.error(
-          'There has been a problem with your fetch operation:',
-          error,
-        );
-      });
+      } catch (error) {
+        setError(`Error during component mount: ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchFeedbackItems();
   }, []);
 
   return (
     <ol className='feedback-list'>
-      {feedbackItems &&
+      {isLoading && <Spinner />}
+
+      {error && <ErrorMessage message={error} />}
+
+      {feedbackItems?.length === 0 && 'Keine Feedbacks vorhanden.'}
+
+      {feedbackItems?.length > 0 &&
         feedbackItems.map((item) => <FeedbackItem key={item.id} item={item} />)}
     </ol>
   );
